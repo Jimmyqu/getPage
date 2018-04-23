16d8767d873e1d5d7ae24e508fb869814a4266f7
qujunjie <laojie420@163.com>
2018-04-23 17:30:38 +0800

    card

--- src/components/child-com/right.vue
+++ src/components/child-com/right.vue
@@ -1,6 +1,19 @@
 <template>
     <div id="right">
-      right
+      <el-row :gutter="20" >
+        <el-col :span="6"  v-for="i in [1,2,3,4]">
+          <el-card :body-style="{ padding: '0px' }"  shadow="hover">
+            <div style="padding: 14px;">
+              <span>好吃的汉堡</span>
+              <div class="bottom clearfix">
+                <el-button type="text" class="button">点击添加到收银台</el-button>
+              </div>
+            </div>
+          </el-card>
+        </el-col>
+
+
+      </el-row>
     </div>
 </template>
 
